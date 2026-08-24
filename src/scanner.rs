50e3f33ca37b6a8ee1830710bd78e7a54a59f134
Med-Fedi-Adel <mohamedfedi.adel@insat.ucar.tn>
2026-08-24 17:44:53 +0100

    scan struct

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,17 +1,42 @@
-use crate::token::{Literal, Token, TokenType};
+use crate::token::{Token, TokenType};
 
 pub struct Scanner {
-    // ...
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
 }
 
 impl Scanner {
     pub fn new(source: &str) -> Self {
-        // ...
+        Scanner {
+            source: String::from(source),
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
     }
 
     pub fn scan_tokens(&mut self) -> Vec<Token> {
-        // ...
+        while !self.is_at_end() {
+            // beginning of the next lexeme
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens
+            .push(Token::new(TokenType::Eof, String::new(), None, self.line));
+
+        std::mem::take(&mut self.tokens)
     }
 
-    // ...
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.chars().count()
+    }
+
+    fn scan_token(&mut self) {
+        todo!()
+    }
 }
